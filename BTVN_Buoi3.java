198bc6a15820e831a48ddd2837a220ab49046b47
MinhLe2909 <76719686+MinhLe2909@users.noreply.github.com>
2024-10-06 12:26:32 +0700

    Add files via upload

---
+++ BTVN_Buoi3.java
@@ -0,0 +1,23 @@
+package BTVN_Buoi3;
+
+import java.awt.event.KeyEvent;
+import java.util.Scanner;
+
+public class BTVN_Buoi3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Mời bạn nhập số: ");
+        int n = scanner.nextInt();
+        if (n<=0)
+        {
+            System.out.println("Mời bạn nhập lại số khác");
+        }
+        else {
+            int mul = 1;
+            for (int i = 2; i <= n; i++) {
+                mul = mul * i;
+            }
+            System.out.println("Tích các số từ 1 đến số bạn vừa nhập = " + mul);
+        }
+    }
+}
