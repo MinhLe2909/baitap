282ee9760abcd1a3348c017f387860c5bbe4f32c
MinhLe2909 <76719686+MinhLe2909@users.noreply.github.com>
2024-10-10 16:01:55 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,42 @@
+package Ontap.baitapATM;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Mời nhập username: ");
+        String username = scanner.nextLine();
+        System.out.println("Mời nhập password: ");
+        String password = scanner.nextLine();
+
+        Account account = new Account("techmaster","hoclacoviec",1000000);
+
+        if(username.equals("techmaster") && password.equals("hoclacoviec")) {
+            System.out.println("login successful");
+            System.out.println("-----MENU-----");
+            System.out.println("1. Xem thông tin tài khoản.");
+            System.out.println("2. Thoát.");
+            System.out.println("Mời chọn chức năng");
+            int chosse = Integer.parseInt(scanner.nextLine());
+            switch(chosse) {
+                case 1:
+                    System.out.println("------Thông tin tài khoản-----");
+                    System.out.println("Tên: "+account.getUsername());
+                    System.out.println("Số dư hiện tại: "+account.getBalance()+"VNĐ");
+                    break;
+                case 2:
+                    break;
+                default:
+
+            }
+
+
+
+        }else {
+            System.out.println("Login Failed");
+        }
+    }
+
+}
